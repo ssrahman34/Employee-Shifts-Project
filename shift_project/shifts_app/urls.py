acdee5536d1eb8b2292a0dee5a2114e5b099ab80
Sarah <sarah.gateway@yahoo.com>
2017-11-16 13:29:53 -0800

    connected shifts_apps page

--- shift_project/shifts_app/urls.py
+++ shift_project/shifts_app/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.shift, name='shift'), #only an empty string will match
+    #url(r'^$', views.shift, name='shift'), #only an empty string will match
+     url(r'^$', views.index, name='index'),
 ]
+
+
--- shift_project/shifts_app/views.py
+++ shift_project/shifts_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-def shift(request):
-    return render(request, 'shifts_app/shift.html')
+#def shift(request):
+ #   return render(request, 'shifts_app/shift.html')
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
