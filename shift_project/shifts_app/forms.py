2fc39e94ab7aeaa0f96ee1b42de29d6e960fdbbb
Sarah <sarah.gateway@yahoo.com>
2017-12-07 13:05:10 -0800

    added validations

--- shift_project/shifts_app/forms.py
+++ shift_project/shifts_app/forms.py
@@ -6,4 +6,12 @@ class RunForm(forms.ModelForm):
         model = Run
         fields = ['user_id', 'start_datetime', 'end_datetime']
         #default_validators = [validators.validate_run],
+    def clean(self):
+        cleaned_data = super(RunForm, self).clean()
+        start_time = cleaned_data['start_datetime']
+        end_time = cleaned_data['end_datetime']
+        if start_time and end_time:
+            if end_time.time() < start_time.time():
+                raise forms.ValidationError("*Note: End time cannot be earlier than start time! Please try again.")
+        return cleaned_data
         
