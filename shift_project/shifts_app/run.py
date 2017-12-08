057cbc1b23b4c7778c44550f831bbe7f1e279951
Sarah <sarah.gateway@yahoo.com>
2017-12-08 12:50:11 -0800

    created history grouping

--- shift_project/shifts_app/run.py
+++ shift_project/shifts_app/run.py
@@ -1,12 +1,6 @@
 from django.db import models
+from shifts_app.his import His
 from django.core.urlresolvers import reverse
-
-#from shifts_app.shift import Shift
-"""class RunManager(models.Manager):
-  def create_run(self, user_id, start_datetime, end_datetime, shift): #pass in shift
-        run = Run(user_id = user_id, start_datetime= start_datetime, end_datetime= end_datetime, shift = shift) #assign to given parameters
-        
-        run.save()"""
 
 
 class Run(models.Model):
@@ -14,12 +8,13 @@ class Run(models.Model):
     db_table="run"
 
     shift = models.ForeignKey("Shift", null = True, blank= True, related_name="runs_related", on_delete = models.CASCADE) 
-    #tells django each run is realted to a single shift
+    #history = models.OneToOneField("His", null = True, blank= True, related_name="history", on_delete = models.CASCADE)
     user_id = models.IntegerField(default=0, blank=True)
-
+     
     start_datetime = models.DateTimeField()
     end_datetime = models.DateTimeField()
     covered = models.BooleanField(default=False)
+    
     @classmethod
     def create(cls, user_id, start_datetime, end_datetime, shift):
         run = cls(user_id=user_id, start_datetime=start_datetime, end_datetime=end_datetime, shift=shift)
